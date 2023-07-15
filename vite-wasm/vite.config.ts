03dc5634a58e939813a7b34ed4955ef96a8250ce
yexiyue <yexiyue666@qq.com>
2023-07-15 16:23:48 +0800

    fix: 修改打包路径

--- vite-wasm/vite.config.ts
+++ vite-wasm/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import topLevelAwait from 'vite-plugin-top-level-await'
 export default defineConfig({
-  plugins:[topLevelAwait()],
+  plugins: [topLevelAwait()],
+  base:'/wasm-game-of-life',
   optimizeDeps: {
     exclude: ["wasm-game-of-life"],
   },
   build: {
-    outDir:"../docs"
+    outDir: "../docs",
   }
 });
